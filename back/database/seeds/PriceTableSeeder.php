97c8bbdb09b4ecb69b743583dfbcd05bb84f48a0
DmitriyCub <dmitriy.hladyshev@qbex.io>
2019-03-19 08:18:59 +0000

    backup commit

--- back/database/seeds/PriceTableSeeder.php
+++ back/database/seeds/PriceTableSeeder.php
@@ -15,7 +15,9 @@ class PriceTableSeeder extends Seeder
 	DB::table('price')->insert([
             [
                 'id' => '1',
-		'value' => '7.00',
+		        'value' => '7.00',
+                'language_id' => '1',
+                'currency' => 'dollars',
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ]
