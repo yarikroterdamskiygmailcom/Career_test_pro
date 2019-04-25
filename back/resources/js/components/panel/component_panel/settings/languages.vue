d9fe3f72267ecd6aee105c3a8455475aa2d2597a
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2019-04-25 17:07:37 +0300

    add export

--- back/resources/js/components/panel/component_panel/settings/languages.vue
+++ back/resources/js/components/panel/component_panel/settings/languages.vue
@@ -12,17 +12,37 @@
                 <td width="15">#</td>
                 <td width="200">Language</td>
                 <td width="300">Code</td>
-                <td width="300">status</td>
+                <td width="300">Status</td>
+                <td width="300">Edit status</td>
+                <td width="300" style="padding-left: 50px">Import</td>
+                <td width="300" style="padding-left: 50px">Export</td>
             </tr>
             </thead>
             <tbody style="width: 915px">
             <tr v-for="(item, index) in languages_list">
                 <td>{{index + 1}}</td>
                 <td>{{item.language}}</td>
-                <td>{{item.code}}</td>
-                <td>{{item.status}}</td>
+                <td >{{item.code}}</td>
+                <td :style="{color: item.status ? 'green' : 'red'}">{{item.status}}</td>
                 <td>
-                    <v-btn color="primary" v-if="item.id == 1" flat @click="exportCSV(item.id)">
+                    <v-btn color="primary"
+                        flat
+                        @click="edit_status(item.language, item.code,  item.status ? 0 : 1)">
+                        Edit status
+                    </v-btn>
+                </td>
+                <td>
+                    <v-btn color="primary"
+                       flat
+                       @click="importCSV(item.id)">
+                       Import CSV
+                    </v-btn>
+                </td>
+                <td>
+                    <v-btn color="primary"
+                        v-if="item.id == 1"
+                        flat
+                        @click="exportCSV(item.id)">
                         Export CSV
                     </v-btn>
                 </td>
@@ -73,10 +93,55 @@
                 </v-card-text>
 
                 <v-divider></v-divider>
+
                 <v-card-actions>
                     <v-spacer></v-spacer>
                     <v-btn color="primary" flat @click="dialog1 = !dialog1">
                         Close
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialog2" width="500">
+            <v-card>
+                <v-card-title class="headline grey lighten-2" primary-title>
+                    <div style="margin-right: 20px">Download CSV</div>
+                </v-card-title>
+                <v-card-text>
+                    <v-flex xs12 class="text-xs-center text-sm-center text-md-center text-lg-center">
+                        <v-text-field
+                                label="Report"
+                                @click="pickFile('Report')"
+                                v-model='dialog2_data.report.imageName'
+                                prepend-icon='attach_file'></v-text-field>
+                        <input
+                                type="file"
+                                style="display: none"
+                                ref="Report"
+                                @change="onFilePicked($event, 'report')"
+                        >
+                    </v-flex>
+                    <v-flex xs12 class="text-xs-center text-sm-center text-md-center text-lg-center">
+                        <v-text-field
+                                label="Site"
+                                @click="pickFile('Site')"
+                                v-model='dialog2_data.site.imageName'
+                                prepend-icon='attach_file'></v-text-field>
+                        <input
+                                type="file"
+                                style="display: none"
+                                ref="Site"
+                                @change="onFilePicked($event, 'site')"
+                        >
+                    </v-flex>
+                </v-card-text>
+
+                <v-divider></v-divider>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary" flat @click="ImportFinalCSV()">
+                        Import Final CSV
                     </v-btn>
                 </v-card-actions>
             </v-card>
@@ -102,8 +167,22 @@
                     status:  0
                 },
                 dialog1:false,
+                dialog2:false,
                 dialog1_data: {},
-                docs:new FormData()
+                dialog2_data: {
+                    id:'',
+                    report:{
+                        imageName: '',
+                        imageUrl: '',
+                        imageFile: ''
+                    },
+                    site:{
+                        imageName: '',
+                        imageUrl: '',
+                        imageFile: ''
+                    }
+                },
+                docs:new FormData(),
             }
         },
         created() {
@@ -117,20 +196,40 @@
             save(){
                 if(!this.data.language && !this.data.code) return;
                 this.addOrUpdateLanguage()
-                    .then(res => {
-                        console.log(res);
-                        // return this.addDocument()
+                    .then(res =>{
+                        this.dialog = false;
+                        this.getLanguage()
                     })
+            },
+            edit_status(language, code, status){
+                this.data.language = language;
+                this.data.code = code;
+                this.data.status = status;
+                this.addOrUpdateLanguage()
                     .then(res => {
-                        // this.dialog = !this.dialog;
-                        // this.getLanguage()
+                        this.data.language = '';
+                        this.data.code = '';
+                        this.data.status = 0;
+                        this.getLanguage()
                     })
             },
             addOrUpdateLanguage(){
                 return Settings.add_language(this.data)
             },
-            addDocument(){
-                return Settings.add_Document(this.docs)
+            ImportFinalCSV(){
+
+                let data = new FormData();
+                data.append('import_report', this.dialog2_data.report.imageFile);
+                data.append('import_site',   this.dialog2_data.site.imageFile);
+
+                Settings.add_Document(data, this.dialog2_data.id)
+                    .then(res => {
+                        Object.keys(this.dialog2_data.report).forEach(item => {
+                            this.dialog2_data.report[item] = null;
+                            this.dialog2_data.site[item] = null;
+                        });
+                        this.dialog2 = false;
+                    })
             },
             exportCSV(id){
                 Settings.export(id)
@@ -140,8 +239,29 @@
                     })
             },
             download(name){
-                window.location.href = this.dialog1_data[name]
+                window.location.href = this.dialog1_data[name];
                 this.dialog1 = !this.dialog1;
+            },
+            pickFile (name) {
+                this.$refs[name].click()
+            },
+            onFilePicked (e, type) {
+                const files = e.target.files;
+                if(files[0] !== undefined) {
+                    this.dialog2_data[type].imageName = files[0].name;
+                    if(this.dialog2_data[type].imageName.lastIndexOf('.') <= 0) {
+                        return
+                    }
+                    this.dialog2_data[type].imageFile = files[0]
+                } else {
+                    this.dialog2_data[type].imageName = '';
+                    this.dialog2_data[type].imageFile = '';
+                    this.dialog2_data[type].imageUrl = '';
+                }
+            },
+            importCSV(id){
+                this.dialog2 = !this.dialog2;
+                this.dialog2_data.id = id;
             }
         }
     }
