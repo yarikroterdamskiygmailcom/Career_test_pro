ff5e7912d91bf3fa9bfd08a279d5e8793d2d4ab7
DmitriyCub <dmitriy.hladyshev@qbex.io>
2019-06-10 16:53:13 +0300

    maintenance mode

--- back/app/Http/Controllers/API/StripePaymentController.php
+++ back/app/Http/Controllers/API/StripePaymentController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use Session;
 use Stripe;
 use Illuminate\Support\Facades\Input;
+use App\Model\Customer;
+use App\Model\Price;
 
 
 class StripePaymentController extends Controller
@@ -32,16 +34,27 @@ class StripePaymentController extends Controller
      */
     public function stripePost(Request $request)
     {
+        $resultData = Session::get('result');
+        $customerData = json_decode(base64_decode($resultData), true);
+
         Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
         Stripe\Charge::create ([
-            "amount" => 39.95*100,
+            "amount" => $customerData['price']*100,
             "currency" => "usd",
             "source" => $request->stripeToken,
             "description" => "Payment for careertestpro PDF report"
         ]);
+        Session::forget('result');
+        $secretLink = md5($resultData . md5($resultData));
+        $newCustomer['customer'] = $resultData;
+        $newCustomer['price'] = $customerData['price'];
+        $newCustomer['payment_system'] = 'stripe';
+        $newCustomer['secret_link'] = $secretLink;
 
-        Session::flash('success', 'Payment successful!');
+        $customer = Customer::create($newCustomer);
+        return redirect()->route('send-pdf', array('customer_id' => $customer->id, 'result_token' => $secretLink, 'customer_email' => $customerData['email']));
 
-        return back();
+        //Session::flash('success', 'Payment successful!');
+        //return back();
     }
 }
